6b409b6ee43293d3627b94e164de3b8953be1319
BeheraRatna <ratna12behera@gmail.com>
2021-09-27 12:18:01 +0530

    Exception Code

---
+++ src/Demoprogram.java
@@ -0,0 +1,56 @@
+
+public class Demoprogram {
+	/*String name;
+	String type;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demoprogram d1 = new Demoprogram();
+		d1.name = "apple";
+		d1.type = "foods";
+		Demoprogram d2 = new Demoprogram();
+		d2.name = "baby";
+		d2.type = "Human beings";
+		System.out.println(d2.name);*/
+	
+	String name;
+	String type;
+	void category()
+	{
+		name="raju";
+		type="good";
+		System.out.println(name+" "+"is a"+" "+type+" "+"boy");
+	}
+	void category1() {
+		name="pawan";
+		type="bad";
+		System.out.println(name+" "+"is a"+" "+type+" "+"boy");
+			
+	}
+	
+	public static void main(String []args) {
+		System.out.println("main start");
+		Demoprogram d1=new Demoprogram();
+		d1.category();
+		d1.category1();
+		System.out.println("End main");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+
+	}
+
+}
